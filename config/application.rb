d11b4483e08dfc07ac8b6edf6f03b8e0ba575b27
saianirudh123 <saianirudh196@gmail.com>
2015-08-04 22:37:32 +0530

    Devise user added

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Pintresting
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -22,5 +22,9 @@ module Pintresting
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #required for heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for heroku
+  #set to actual host name
+  config.action_mailer.default_url_options = { :host => 'https://arra-pinteresting.herokuapp.com/' }
 end
